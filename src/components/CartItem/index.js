803eb85da19158a6b3261a09f4d37ce3f79740ab
mbssilva <mbernardi.s@outlook.com>
2020-03-19 14:19:32 -0300

    CartItem com alterações na estilização. Ainda não está pronto

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 import {
   Wrapper,
@@ -8,35 +8,46 @@ import {
   ProductTitle,
   ProductPrice,
   Trash,
+  TrashContainer,
 } from './styles';
 
 export default function Header() {
   return (
-    <>
+    <View style={{ backgroundColor: 'red' }}>
       <Wrapper>
         <Image
           source={{
             uri:
               'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
           }}
-          style={{ width: 105, height: 105, marginTop: 5 }}
+          style={{ width: 105, height: 105, margin: 5 }}
         />
         <Container>
           <ProductTitle>
-            <Text>Tênis de Caminhada Leve Confortáve</Text>
+            <Text numberOfLines={3} style={{ fontSize: 15 }}>
+              Tênis de Caminhada Leve Confortáve de Passeio
+            </Text>
           </ProductTitle>
           <ProductPrice>
-            <Text>R$ 179,98</Text>
+            <Text
+              numberOfLines={3}
+              style={{
+                fontSize: 17,
+                fontWeight: 'bold',
+              }}
+            >
+              R$ 179,98
+            </Text>
           </ProductPrice>
         </Container>
-        <TouchableOpacity>
+        <TrashContainer>
           <Trash />
-        </TouchableOpacity>
+        </TrashContainer>
       </Wrapper>
 
       <AmountAndPrice>
         <Text>This is an amount component</Text>
       </AmountAndPrice>
-    </>
+    </View>
   );
 }
--- src/components/CartItem/styles.js
+++ src/components/CartItem/styles.js
@@ -6,29 +6,47 @@ export const Wrapper = styled.View`
   flex-direction: row;
   background-color: #fff;
   border-radius: 5px;
+  height: 120px;
+  width: auto;
   margin: 5px;
 `;
 
 export const Container = styled.View`
   flex-direction: column;
-  background-color: #fff;
+  justify-content: space-evenly;
+
   border-radius: 8px;
-  margin: 0 10px;
-  align-items: center;
+  width: 220px;
+  margin: 5px 10px 5px 2px;
+`;
+
+export const ProductTitle = styled.View`
+  flex: 1;
+  margin: 5px 10px 5px 2px;
+  justify-content: center;
+`;
+
+export const ProductPrice = styled.View`
+  flex: 1;
+  margin: 5px 10px 5px 2px;
+  /* justify-content: center; */
 `;
 
 export const AmountAndPrice = styled.View`
   flex-direction: column;
-  background-color: #fff;
+  background-color: #eee;
   align-items: center;
 `;
 
-export const ProductTitle = styled.View``;
-
-export const ProductPrice = styled.View``;
-
 export const Trash = styled(Icon).attrs({
   name: 'delete-forever',
-  size: 24,
+  size: 28,
   color: '#7159c1',
 })``;
+
+export const TrashContainer = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  margin: 44px 7px;
+`;
